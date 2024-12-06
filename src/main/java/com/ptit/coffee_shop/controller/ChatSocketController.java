5c27d9fbe5bf656ffd4e8316b0b8a234c21e0d91
Truong <truongnguyendac002@gmail.com>
2024-12-06 23:06:31 +0700

    update chat chit, lan 1, con loi user offline ko nhan tin duoc

---
+++ src/main/java/com/ptit/coffee_shop/controller/ChatSocketController.java
@@ -0,0 +1,65 @@
+package com.ptit.coffee_shop.controller;
+
+import com.ptit.coffee_shop.common.Constant;
+import com.ptit.coffee_shop.common.GsonUtil;
+import com.ptit.coffee_shop.config.MessageBuilder;
+import com.ptit.coffee_shop.exception.CoffeeShopException;
+import com.ptit.coffee_shop.payload.request.ChatMessageRequest;
+import com.ptit.coffee_shop.payload.response.ChatMessageResponse;
+import com.ptit.coffee_shop.payload.response.RespMessage;
+import com.ptit.coffee_shop.service.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class ChatSocketController {
+    public final ChatService chatService;
+    public final MessageBuilder messageBuilder;
+    public final SimpMessagingTemplate simpMessagingTemplate;
+    @MessageMapping("/chat/{conversationId}")
+    @SendTo("/topic/conversation/{conversationId}")
+    public String sendMessage(
+            @DestinationVariable long conversationId,
+            ChatMessageRequest message) {
+        try {
+            RespMessage conversationResponse = chatService.updateConversation(message, conversationId);
+            String response = GsonUtil.getInstance().toJson(conversationResponse);
+            return response;
+        } catch (CoffeeShopException exception) {
+            RespMessage respMessage = messageBuilder.buildFailureMessage(exception.getCode(), exception.getObjects(), exception.getMessage());
+            return GsonUtil.getInstance().toJson(respMessage);
+
+        } catch (Exception e) {
+            RespMessage respMessage = messageBuilder.buildFailureMessage(Constant.SYSTEM_ERROR, null, e.getMessage());
+            return GsonUtil.getInstance().toJson(respMessage);
+        }
+    }
+
+    @MessageMapping("/chat/admin")
+    @SendTo("/topic/admin")
+    public String sendToAdmin(
+            String message) {
+        try {
+            log.info("Message from client: " + message);
+            return message;
+        } catch (CoffeeShopException exception) {
+            RespMessage respMessage = messageBuilder.buildFailureMessage(exception.getCode(), exception.getObjects(), exception.getMessage());
+            return GsonUtil.getInstance().toJson(respMessage);
+
+        } catch (Exception e) {
+            RespMessage respMessage = messageBuilder.buildFailureMessage(Constant.SYSTEM_ERROR, null, e.getMessage());
+            return GsonUtil.getInstance().toJson(respMessage);
+        }
+    }
+
+
+
+}
