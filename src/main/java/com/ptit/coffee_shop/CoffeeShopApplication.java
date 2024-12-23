32934fc692cad04510986f4ca082b9c3b3954bff
Truong <truongnguyendac002@gmail.com>
2024-12-23 22:25:29 +0700

    them gio dia phuong

--- src/main/java/com/ptit/coffee_shop/CoffeeShopApplication.java
+++ src/main/java/com/ptit/coffee_shop/CoffeeShopApplication.java
@@ -1,16 +1,25 @@
 package com.ptit.coffee_shop;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class CoffeeShopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeShopApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Đặt múi giờ mặc định là GMT+7 (Asia/Ho_Chi_Minh)
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 	}
 
 
